6a59ff4957d91e8c2c0633a268084d4dd64a3cff
Ali <aalj011@gmail.com>
2015-12-27 21:05:51 +1300

    initial commit nothing done yet

---
+++ fsxsim/fsxsim/fsxsim.c
@@ -0,0 +1,16 @@
+/*
+ * fsxsim.c
+ *
+ * Created: 27/12/2015 9:04:39 p.m.
+ *  Author: Ali
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
